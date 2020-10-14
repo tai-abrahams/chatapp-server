56cc01b88545b75091cba4eb27da624a19df1fe6
tai-abrahams <taiwo.abrahams@icloud.com>
2020-10-14 13:52:13 +0100

    Update credential-router.js

--- routes/credential-router.js
+++ routes/credential-router.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 
 router.post('/cred-create', credCtrl.createUser) 
-router.put('/cred-upd/:id', credCtrl.updateUser) //:id used to be able to store the id from the client in the request line
+router.put('/cred-upd/:id', credCtrl.updateUser)
 router.delete('/cred-del/:id', credCtrl.deleteUser)
 router.post('/loginCred', credCtrl.getUserByUser)
 
