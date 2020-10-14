bceeb77651a826041c977c61acb5c3f284b8361a
tai-abrahams <taiwo.abrahams@icloud.com>
2020-10-14 13:46:30 +0100

    Update credentials-model.js

--- models/credentials-model.js
+++ models/credentials-model.js
@@ -3,7 +3,6 @@ const UserSchema = mongoose.Schema
 const FriendSchema = './friend-model'
 const mongooseUniqueValidator = require('mongoose-unique-validator')
 
-
 const bcrypt = require('bcrypt')
 const Friend = require('./friend-model')
 
@@ -18,9 +17,6 @@ const user = new UserSchema({
 
 user.plugin(mongooseUniqueValidator)
 
-
-
-
 user.pre('save', function (next) {
   var user = this;
   bcrypt.hash(user.password, 10, function (err, hash) {
@@ -32,31 +28,6 @@ user.pre('save', function (next) {
   })
 });
 
-
 const User =  mongoose.model('User', user) 
 
-
-
 module.exports = User
-
-
-
-// user.pre('save', async function(){
-//     //pre is middleware for mongoose. There are 4 types of middleware, document middleware being one. 'Save' is a document middleware function. So when using 'this' with a document middleware function such as 'save' or 'remove'. 'This' refers to the document.
-//     //There are also query middleware functions, aggregate and model middleware functions.
-//     const user = this; 
-// //bcrypt.hash used to hash/encode the password for security reasons
-  
-//    async function hashPassword(user){
-//        const password = user.password
-//        const saltRounds = 10;
-
-//        const hashedPassword = await new Promise((resolve, reject)=>{
-//            bcrypt.hash(password, saltRounds, (err, hash)=>{
-//                if(err) reject(err)
-//                resolve(hash)
-//            });
-//        })
-//        return hashedPassword
-//    } 
-// })
